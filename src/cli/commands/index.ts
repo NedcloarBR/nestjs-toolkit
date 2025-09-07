e97f2f807e26ac2802db765bc7661cd3a447386a
NedcloarBR <nedcloar1@hotmail.com>
2025-09-07 15:17:58 -0300

    feat: update barrel files

--- src/cli/commands/index.ts
+++ src/cli/commands/index.ts
@@ -1,5 +1,2 @@
 export { InitCommand } from "./init.command";
-export { APPKeyCommand } from "./key/app-key.command";
-export { CookieKeyCommand } from "./key/cookie-key.command";
-export { JwtKeyCommand } from "./key/jwt-key.command";
-export { SessionKeyCommand } from "./key/session-key.command";
+export * from "./key";
---
+++ src/cli/commands/key/index.ts
@@ -0,0 +1,4 @@
+export { APPKeyCommand } from "./app-key.command";
+export { CookieKeyCommand } from "./cookie-key.command";
+export { JwtKeyCommand } from "./jwt-key.command";
+export { SessionKeyCommand } from "./session-key.command";
